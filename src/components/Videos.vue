ce5f0e0984cdeb30a2e2c5133f8e11ca5724c292
u22f67 <tamas.maylander@avl.com>
2019-03-12 14:44:36 +0100

    saving video links to backend, and loading it from backend

--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -1,8 +1,7 @@
 <template>
-<div class="row">
-  {{arr}}
-  <div class="content-div col-12 col-lg-5" v-for="(videoLink,index) in videosLinks" :key="index">
-    <iframe width="100%" height="100%" :src="videoLink" frameborder="0" 
+<div class="row padding-container">
+  <div class="content-div col-12 col-lg-5 mt-md-0 mt-lg-0" v-bind:class="getClass(index)" v-for="(videoLink,index) in videosLinks" :key="index">
+    <iframe width="100%" height="100%" :src="videoLink" frameborder="0"
     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
   </div>
@@ -18,21 +17,30 @@ export default {
   data () {
     return {
       videosLinks: [],
-      arr: null
+      asd: null,
+      isActive: true
     }
   },
    mounted () {
- 
-    Axios.get('https://www.googleapis.com/youtube/v3/search?part=id&channelId=UCIiJ33El2EakaXBzvelc2bQ&type=video&key=AIzaSyBfynihkI2OuWswP76U1fhCGdlX3EzSELE')
+      Axios.get('http://localhost:3000/videos').then((response) => {
+        this.videosLinks = response.data.links;
+        if (this.videosLinks.length <= 0) {
+          Axios.get('https://www.googleapis.com/youtube/v3/search?part=id&channelId=UCIiJ33El2EakaXBzvelc2bQ&type=video&key=AIzaSyBfynihkI2OuWswP76U1fhCGdlX3EzSELE')
           .then(response => {
              response.data.items.forEach(element => {
               this.videosLinks.push('https://www.youtube.com/embed/'+element.id.videoId);
              });
 
+          Axios.post('http://localhost:3000/videos', {"links": this.videosLinks});
           });
-           const data = JSON.stringify("asfafasfa")
-        window.localStorage.setItem('arr', data);
-   this.arr= window.localStorage.getItem('arr');
+        }
+      })
+      .catch(() => {})
+  },
+  methods: {
+    getClass(property) {
+      return property % 2 === 0 ? 'top-padding-minus' : ''
+    }
   }
 }
 </script>
@@ -41,6 +49,15 @@ export default {
 <style scoped>
 @import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 
+@media (min-width: 991.98px) { 
+  .padding-container {
+    margin-top: 10% !important;
+  }
+
+  .top-padding-minus {
+    margin-top: -10% !important;
+  }
+}
 .content-div {
   border-radius: 25px;
   background-color: grey;
